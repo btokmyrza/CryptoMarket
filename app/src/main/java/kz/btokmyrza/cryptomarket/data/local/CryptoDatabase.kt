b060bfe0b5dba222a6ce6e800fd141cfbe1b854c
azamat.alisher <azamat.alisher.1999@gmail.com>
2022-07-22 12:43:30 +0600

    [FIXED] room database

--- app/src/main/java/kz/btokmyrza/cryptomarket/data/local/CryptoDatabase.kt
+++ app/src/main/java/kz/btokmyrza/cryptomarket/data/local/CryptoDatabase.kt
@@ -1,12 +1,13 @@
 package kz.btokmyrza.cryptomarket.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(
     entities = [CryptoEntity::class],
     version = 1
 )
-abstract class CryptoDatabase {
+abstract class CryptoDatabase : RoomDatabase() {
 
     abstract val dao: CryptoDao
 
